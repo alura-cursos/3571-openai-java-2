998a90063c6a1543a50164589ccc5d5862152ddd
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-12-06 21:25:50 -0300

    1.3 - Integrando com a API

---
+++ src/main/java/br/com/alura/ecomart/chatbot/domain/service/ChatbotService.java
@@ -0,0 +1,22 @@
+package br.com.alura.ecomart.chatbot.domain.service;
+
+import br.com.alura.ecomart.chatbot.infra.openai.DadosRequisicaoChatCompletion;
+import br.com.alura.ecomart.chatbot.infra.openai.OpenAIClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatbotService {
+
+    private OpenAIClient client;
+
+    public ChatbotService(OpenAIClient client) {
+        this.client = client;
+    }
+
+    public String responderPergunta(String pergunta) {
+        var promptSistema = "Você é um chatbot de atendimento a clientes de um ecommerce e deve responder apenas perguntas relacionadas com o ecommerce";
+        var dados = new DadosRequisicaoChatCompletion(promptSistema, pergunta);
+        return client.enviarRequisicaoChatCompletion(dados);
+    }
+
+}
--- src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
+++ src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecomart.chatbot.web.controller;
 
+import br.com.alura.ecomart.chatbot.domain.service.ChatbotService;
 import br.com.alura.ecomart.chatbot.web.dto.PerguntaDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,12 @@ public class ChatController {
 
     private static final String PAGINA_CHAT = "chat";
 
+    private ChatbotService service;
+
+    public ChatController(ChatbotService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public String carregarPaginaChatbot() {
         return PAGINA_CHAT;
@@ -18,7 +25,7 @@ public class ChatController {
     @PostMapping
     @ResponseBody
     public String responderPergunta(@RequestBody PerguntaDto dto) {
-        return dto.pergunta();
+        return service.responderPergunta(dto.pergunta());
     }
 
     @GetMapping("limpar")
